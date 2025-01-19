7ecc56b2d63b0a80e25ed9aac758389529bec287
MarcusGanea <ganea.marcus@gmail.com>
2025-01-19 21:02:40 +0100

    ez

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = [os.getenv('PROJECT_DOMAIN') + ".glitch.me"]
 
 
 # Application definition
