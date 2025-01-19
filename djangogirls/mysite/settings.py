2c5b5e0c61f96fdaa2b7d804a7006acb9ba7d813
MarcusGanea <ganea.marcus@gmail.com>
2025-01-19 21:00:59 +0100

    added secret

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2uh1&v#5iexm@a-=jz6&nfqg!)s5ybdb-t#_lhj09(t@xia9w9"
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
